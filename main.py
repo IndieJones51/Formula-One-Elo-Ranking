0d406d1d302d4983dc55f693652184c7514ee3ad
ixb19173 <cameron.wailly.2019@uni.strath.ac.uk>
2023-05-12 18:14:26 +0100

    extracting driver details

--- main.py
+++ main.py
@@ -12,4 +12,31 @@ results_df = results_df[['resultId','raceId','driverId','constructorId','grid','
 results_df = pd.merge(results_df, drivers_df, how='left', on='driverId')
 results_df = pd.merge(results_df, constructors_df, how='left', on='constructorId')
 
-print(results_df.head(10))
+
+drivers = results_df['driverId'].value_counts().keys()
+
+
+for driver in drivers:
+
+    driver_df = results_df[results_df['driverId'] == driver]
+
+    races = driver_df['raceId'].value_counts().keys()
+    teams = driver_df['constructorId'].value_counts().keys()
+
+    for race in races:
+
+        for team in teams:
+
+            race_df = driver_df[(driver_df['raceId'] == race) & (driver_df['constructorId'] == team)]
+            opponent_df = results_df[(results_df['driverId'] != driver) & (results_df['raceId'] == race) & (results_df['constructorId'] == team)]
+
+            if len(race_df) != 0 and len(opponent_df) != 0:
+
+                print(race_df)
+                print(opponent_df)
+
+
+
+
+
+
