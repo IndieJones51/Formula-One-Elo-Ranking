42669c0916d90ee1c31599dda8cda5594264b868
ixb19173 <cameron.wailly.2019@uni.strath.ac.uk>
2023-05-18 22:59:38 +0100

    applying elo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def k_factor(score):
     if score > 2400:
         return 16
@@ -13,61 +14,50 @@ def probability_of_win(current, opponent):
     return 1 / (1 + 10 ** ((opponent - current) / 400))
 
 
-
 drivers_df = pd.read_csv("data/drivers.csv")
-drivers_df = drivers_df[['driverId','driverRef','surname']]
+drivers_df = drivers_df[['driverId', 'driverRef', 'surname']]
+drivers = drivers_df['driverId'].value_counts().keys()
 
 constructors_df = pd.read_csv('data/constructors.csv')
-constructors_df = constructors_df[['constructorId','constructorRef']]
+constructors_df = constructors_df[['constructorId', 'constructorRef']]
 
 results_df = pd.read_csv("data/results.csv")
-results_df = results_df[['resultId','raceId','driverId','constructorId','grid','position','positionText','positionOrder','rank']]
+results_df = results_df[
+    ['resultId', 'raceId', 'driverId', 'constructorId', 'grid', 'position', 'positionText', 'positionOrder', 'rank']
+]
 
 results_df = pd.merge(results_df, drivers_df, how='left', on='driverId')
 results_df = pd.merge(results_df, constructors_df, how='left', on='constructorId')
 
-results_df['Elo'] = 1000
+results_df = results_df.sort_values(by=['driverId'], axis=0, inplace=False)
 
-drivers = results_df['driverId'].value_counts().keys()
+driver_dataframes = [results_df[results_df['driverId'] == id] for id in results_df['driverId'].unique()]
+driver_scores = {"name": results_df['driverRef'].unique(),
+                 "elo_score": [1000 for x in range(len(results_df['driverRef'].unique()))]
+                 }
 
-results_df.to_excel(r'results.xlsx')
+driver_scores = pd.DataFrame(data=driver_scores['elo_score'], index=driver_scores['name'], columns=['elo_score'])
 
+print(driver_scores.tail())
+for driver in driver_dataframes:
 
+    current_name = driver['driverRef'].values[0]
 
-for driver in drivers:
+    for race in driver['raceId'].unique():
+        current_driver = driver[driver['raceId'] == race]
+        opponent_driver = results_df[
+            (results_df['raceId'] == race)
+            & (results_df['constructorId'] == current_driver['constructorId'].values[0])
+            & (results_df['driverId'] != current_driver['driverId'].values[0])
+            ]
 
-    driver_df = results_df[results_df['driverId'] == driver]
+        """print(current_driver['driverRef'].values[0], "Vs", opponent_driver['driverRef'].values[0])
 
-    races = driver_df['raceId'].value_counts().keys()
-    teams = driver_df['constructorId'].value_counts().keys()
+        if current_driver['positionOrder'].values[0] > opponent_driver['positionOrder'].values[0]:
 
-    for race in races:
-
-        for team in teams:
-
-            race_df = driver_df[(driver_df['raceId'] == race) & (driver_df['constructorId'] == team)]
-            opponent_df = results_df[(results_df['driverId'] != driver) & (results_df['raceId'] == race) & (results_df['constructorId'] == team)]
-
-            if len(race_df) != 0 and len(opponent_df) != 0:
-
-                current_elo = race_df['Elo'].values[0]
-                opponent_elo = race_df['Elo'].values[0]
-
-                current_probability = probability_of_win(current_elo, opponent_elo)
-                opponent_probability = 1 - current_probability
-
-                if race_df['positionOrder'].values[0] < opponent_df['positionOrder'].values[0]:
-                    current_elo = current_elo + (k_factor(current_elo) * (1 - current_probability))
-                else:
-                    current_elo = current_elo + (k_factor(current_elo) * (0 - current_probability))
-
-                results_df['Elo'][(results_df['driverId'] == driver) & (results_df['raceId'] == race) & (results_df['constructorId'] == team)] = current_elo
-                print(results_df['Elo'][(results_df['driverId'] == driver) & (results_df['raceId'] == race) & (results_df['constructorId'])])
-                break
-                print(race_df['surname'].values[0], race_df['positionOrder'].values[0], race_df['Elo'].values[0], opponent_df['surname'].values[0], opponent_df['positionOrder'].values[0], race_df['Elo'].values[0])
+            current_elo = driver_scores.loc[[current_name]]
+            print(current_elo)"""
 
 
-
-results_df.to_excel(r'results.xlsx')
 
 
