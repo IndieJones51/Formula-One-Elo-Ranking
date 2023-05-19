06b473a8d14feb777eb77e03abea5e378dd253dc
ixb19173 <cameron.wailly.2019@uni.strath.ac.uk>
2023-05-19 19:26:15 +0100

    applying elo

--- main.py
+++ main.py
@@ -33,12 +33,14 @@ results_df = results_df.sort_values(by=['driverId'], axis=0, inplace=False)
 
 driver_dataframes = [results_df[results_df['driverId'] == id] for id in results_df['driverId'].unique()]
 driver_scores = {"name": results_df['driverRef'].unique(),
-                 "elo_score": [1000 for x in range(len(results_df['driverRef'].unique()))]
+                 "elo_score": [1000 for x in range(len(results_df['driverRef'].unique()))],
+                 "wins": [0 for x in range(len(results_df['driverRef'].unique()))],
+                 "losses": [0 for x in range(len(results_df['driverRef'].unique()))]
                  }
 
 driver_scores = pd.DataFrame(data=driver_scores['elo_score'], index=driver_scores['name'], columns=['elo_score'])
 
-print(driver_scores.tail())
+
 for driver in driver_dataframes:
 
     current_name = driver['driverRef'].values[0]
@@ -51,13 +53,29 @@ for driver in driver_dataframes:
             & (results_df['driverId'] != current_driver['driverId'].values[0])
             ]
 
-        """print(current_driver['driverRef'].values[0], "Vs", opponent_driver['driverRef'].values[0])
+        current_elo = driver_scores['elo_score'].loc[[current_name]].values[0]
 
-        if current_driver['positionOrder'].values[0] > opponent_driver['positionOrder'].values[0]:
+        if len(opponent_driver) != 0:
 
-            current_elo = driver_scores.loc[[current_name]]
-            print(current_elo)"""
+            opponent_elo = driver_scores.loc[[opponent_driver['driverRef'].values[0]]].values[0]
+
+            print(current_driver['driverRef'].values[0], "Vs", opponent_driver['driverRef'].values[0])
 
 
+            if current_driver['positionOrder'].values[0] > opponent_driver['positionOrder'].values[0]:
 
+                current_elo = current_elo + (k_factor(current_elo) * (1 - probability_of_win(current_elo, opponent_elo)))
+                opponent_elo = opponent_elo + (k_factor(opponent_elo) * (0 - probability_of_win(opponent_elo, current_elo)))
 
+                driver_scores.at[current_name, 'elo_score'] = current_elo
+                driver_scores.at[opponent_driver['driverRef'].values[0], 'elo_score'] = opponent_elo
+
+            else:
+
+                current_elo = current_elo + (k_factor(current_elo) * (0 - probability_of_win(current_elo, opponent_elo)))
+                opponent_elo = opponent_elo + (k_factor(opponent_elo) * (1 - probability_of_win(opponent_elo, current_elo)))
+
+                driver_scores.at[current_name, 'elo_score'] = current_elo
+                driver_scores.at[opponent_driver['driverRef'].values[0], 'elo_score'] = opponent_elo
+
+driver_scores.to_excel(r'results.xlsx')
